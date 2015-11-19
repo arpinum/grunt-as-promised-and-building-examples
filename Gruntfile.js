bd7ce932c52896f7d2270c23b6da8519d6fda406
Michael Borde <michael.borde@ubimail.fr>
2015-11-19 23:06:49 +0100

    jshint -> eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,13 @@ var gap = require('grunt-as-promised');
 var shell = require('building').shell;
 
 module.exports = function (grunt) {
-  gap.registerTask(grunt, 'jshint', function () {
-    return shell.execute('jshint', ['.'], {resolveLocalBin: true});
+  gap.registerTask(grunt, 'eslint', function () {
+    return shell.execute('eslint', ['.'], {resolveLocalBin: true});
   });
 
   gap.registerTask(grunt, 'mocha', function () {
     return shell.execute('mocha', ['lib'], {resolveLocalBin: true});
   });
 
-  grunt.registerTask('default', ['jshint', 'mocha']);
+  grunt.registerTask('default', ['eslint', 'mocha']);
 };
